938717046a68356a7c5c693f1a6ef35fbc21b3f0
replakcan <alper.makcan@gmail.com>
2025-04-29 14:26:55 +0300

    Creates todo add button that opens dialog form

---
+++ src/component/TodoAddButton.js
@@ -0,0 +1,17 @@
+function TodoAddButton() {
+  const btn = document.createElement('button')
+
+  btn.classList.add('todo-add-btn')
+  btn.textContent = 'Add Todo'
+
+  btn.addEventListener('click', () => {
+    const dialog = document.querySelector('dialog')
+    dialog.showModal()
+  })
+
+  const sidebar = document.querySelector('.sidebar')
+
+  sidebar.appendChild(btn)
+}
+
+export default TodoAddButton
