759d2a3241cc293689966fbcd60527ac280bbe86
replakcan <alper.makcan@gmail.com>
2025-05-02 14:36:46 +0300

    Updates ui with the new elements

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import TodoAddButton from './component/TodoAddButton'
 import TodoCard from './component/TodoCard'
 import TodoDialog from './component/TodoDialog'
 import TodoForm from './component/TodoForm'
-import { readDataFromLocalStorage } from './lib/readDataFromLocalStorage'
+import ProjectAddButton from './component/ProjectAddButton'
+import ProjectContainer from './component/ProjectContainer'
+import { todoArr } from './todo-array'
+import { projectArr } from './project-array'
+import ProjectCard from './component/ProjectCard'
+import ProjectDialog from './component/ProjectDialog'
+import ProjectForm from './component/ProjectForm'
 
 document.addEventListener('DOMContentLoaded', () => {
-  readDataFromLocalStorage('todos').map((todo) => TodoCard(todo))
+  todoArr.map((todo) => TodoCard(todo))
+  projectArr.map((project) => ProjectCard(project))
 })
 
+ProjectAddButton()
+ProjectDialog(ProjectForm())
+
 TodoDialog(TodoForm())
+
 TodoAddButton()
 
+
+ProjectContainer()
