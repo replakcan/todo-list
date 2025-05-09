eb631e229c1dab40f562350d39a521ce5c25bbc4
replakcan <alper.makcan@gmail.com>
2025-05-09 13:13:23 +0300

    Adds a condition for adding todos
    makes sure there is at least one project

--- src/component/TodoAddButton.js
+++ src/component/TodoAddButton.js
@@ -1,10 +1,15 @@
-function TodoAddButton() {
+function TodoAddButton(projectList) {
   const btn = document.createElement('button')
 
   btn.classList.add('todo-add-btn')
   btn.textContent = 'Add Todo'
 
   btn.addEventListener('click', () => {
+    if (!projectList.length) {
+      alert('You should have at least one project to continue')
+      return
+    }
+
     const dialog = document.querySelector('.todo-dialog')
     dialog.showModal()
   })
